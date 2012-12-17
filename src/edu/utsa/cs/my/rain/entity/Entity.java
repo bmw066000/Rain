173b09d0e3a781c67de5fb04ece6d51f0856b585
Ben Wright <bmw066000@gmail.com>
2012-12-17 09:54:54 -0600

    Episode 39
    Entity Framework

---
+++ src/edu/utsa/cs/my/rain/entity/Entity.java
@@ -0,0 +1,30 @@
+package edu.utsa.cs.my.rain.entity;
+
+import java.util.Random;
+
+import edu.utsa.cs.my.rain.graphics.Screen;
+import edu.utsa.cs.my.rain.level.Level;
+
+public abstract class Entity {
+	
+	public int x, y;
+	private boolean removed = false;
+	protected Level level;
+	protected final Random random = new Random();
+	
+	public void update() {
+	}
+	
+	public void render(Screen screen) {
+	}
+	
+	public void remove() {
+		//Remove from level
+		removed = true;
+	}
+	
+	public boolean isRemoved() {
+		return removed;
+	}
+
+}
