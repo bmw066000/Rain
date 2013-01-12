0e2d31d26edfc441d58c6505896867b82064be0f
Ben Wright <bmw066000@gmail.com>
2013-01-12 17:01:17 -0600

    Episode 48
    Player Direction Animation

--- src/edu/utsa/cs/my/rain/entity/mob/Player.java
+++ src/edu/utsa/cs/my/rain/entity/mob/Player.java
@@ -7,16 +7,18 @@ import edu.utsa.cs.my.rain.input.Keyboard;
 public class Player extends Mob {
 	
 	private Keyboard input;
-	
+	private Sprite sprite;
 	
 	public Player(Keyboard input) {
 		this.input = input;
+		sprite = Sprite.player_forward;
 	}
 	
 	public Player(int x, int y, Keyboard input) {
 		this.x = x;
 		this.y = y;
 		this.input = input;
+		sprite = Sprite.player_forward;
 	}
 	
 	public void update() {
@@ -30,7 +32,11 @@ public class Player extends Mob {
 	}
 	
 	public void render(Screen screen) {
-		screen.renderPlayer(x - 16, y - 16, Sprite.player);
+		if (dir == 0) sprite = Sprite.player_forward;
+		if (dir == 1) sprite = Sprite.player_right;
+		if (dir == 2) sprite = Sprite.player_backward;
+		if (dir == 3) sprite = Sprite.player_left;
+		screen.renderPlayer(x - 16, y - 16, sprite);
 	}
 
 }
--- src/edu/utsa/cs/my/rain/graphics/Sprite.java
+++ src/edu/utsa/cs/my/rain/graphics/Sprite.java
@@ -9,13 +9,12 @@ public class Sprite {
 	
 	public static Sprite grass = new Sprite(16, 2, 0, SpriteSheet.tiles);
 	public static Sprite voidSprite = new Sprite(16, 0x1B87E0);
-
-	public static Sprite player0 = new Sprite(16, 0, 14, SpriteSheet.tiles);
-	public static Sprite player1 = new Sprite(16, 1, 14, SpriteSheet.tiles);
-	public static Sprite player2 = new Sprite(16, 0, 15, SpriteSheet.tiles);
-	public static Sprite player3 = new Sprite(16, 1, 15, SpriteSheet.tiles);
 	
-	public static Sprite player = new Sprite(32, 0, 7, SpriteSheet.tiles);
+	public static Sprite player_forward = new Sprite(32, 0, 6, SpriteSheet.tiles);
+	public static Sprite player_backward = new Sprite(32, 2, 6, SpriteSheet.tiles);
+	public static Sprite player_left = new Sprite(32, 3, 6, SpriteSheet.tiles);
+	public static Sprite player_right = new Sprite(32, 1, 6, SpriteSheet.tiles);
+	
 	
 	public Sprite(int size, int x, int y, SpriteSheet sheet) {
 		SIZE = size;
