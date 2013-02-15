da8089223d8560e9a55ec55e01ee2c583000fd1c
Ben Wright <bmw066000@gmail.com>
2013-02-15 14:17:18 -0600

    Episode 56
    Getting Tiles the Right Way

--- src/edu/utsa/cs/my/rain/level/SpawnLevel.java
+++ src/edu/utsa/cs/my/rain/level/SpawnLevel.java
@@ -9,8 +9,6 @@ import edu.utsa.cs.my.rain.level.tile.Tile;
 
 public class SpawnLevel extends Level {
 
-	private int[] levelPixels;
-
 	public SpawnLevel(String path) {
 		super(path);
 	}
@@ -20,9 +18,8 @@ public class SpawnLevel extends Level {
 			BufferedImage image = ImageIO.read(SpawnLevel.class.getResource(path));
 			int w = image.getWidth();
 			int h = image.getHeight();
-			tiles = new Tile[w * h];
-			levelPixels = new int[w * h];
-			image.getRGB(0, 0, w, h, levelPixels, 0, w);
+			tiles = new int[w * h];
+			image.getRGB(0, 0, w, h, tiles, 0, w);
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("Exception! Could not load level file!");
@@ -33,11 +30,6 @@ public class SpawnLevel extends Level {
 	// Flower = 0xFFFF00
 	// Rock = 0x7F7F00
 	protected void generateLevel() {
-		for (int i = 0; i < levelPixels.length; i++) {
-			if (levelPixels[i] == 0xFF00FF00) tiles[i] = Tile.grass;
-			if (levelPixels[i] == 0xFFFFFF00) tiles[i] = Tile.flower;
-			if (levelPixels[i] == 0xFF7F7F00) tiles[i] = Tile.rock;
-		}
 	}
 
 }
